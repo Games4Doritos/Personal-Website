df4cfd4c5a77bdcd2b6477cc6fe230382035e230
Games4Doritos <evanmiocevich@gmail.com>
2025-10-12 18:58:39 +0800

    Add files via upload

---
+++ Application/backend.py
@@ -0,0 +1,23 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route("/home", methods = ["POST", "GET"])
+def home():
+    return render_template("home.html")
+@app.route("/projects")
+def projects():
+    return render_template('projects.html')
+@app.route("/about")
+def resume():
+    return render_template('about.html')
+
+@app.errorhandler(404)
+def page404(error):
+    return "Error: Detecting multiple NULL class objects in the area. Are you sure whatever you're doing is worth it?"
+
+@app.errorhandler(500)
+def page500(error):
+    return "Something went wrong at Evan Miocevich HQ, we'll get on this (definitely)"
+
+    
