66f8a4542b3d6f5511c1fbfaeeaccfd1d9579323
Games4Doritos <evanmiocevich@gmail.com>
2026-04-17 14:30:52 +0800

    Undo config changes

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/personal-website",
-  images: {
-    unoptimized: true,
-  },
-
 };
 
 export default nextConfig;
